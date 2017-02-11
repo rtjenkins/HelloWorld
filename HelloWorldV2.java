bfb62a7ad1e69688ee78a9040e3891b79646107d
rtjenkins <motphone16@gmail.com>
2017-02-11 04:40:54 -0800

    Rajesh's second commit with java code

---
+++ HelloWorldV2.java
@@ -0,0 +1,13 @@
+public class HelloWorldV2 {
+	public static void main(String[] args) {
+	// Print a message to the terminal window
+	System.out.println("Hello World!!!\n\nThis is Armaan Tulsyani's hello to the entire Earth-1\n\nDo you know there is Earth-2?\n\n");
+	System.out.println("\n\nHello to the entire Universe and beyond.. \n\nActually multi-verse!!!\n\nHa ha ha ha\n\n");
+	int i;
+	for (i=1;i<100;i++) {
+		System.out.println("Hello to Earth-" + i);
+	}
+
+	}
+}
+
