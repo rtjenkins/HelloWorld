e886fb42376276c0223a4adc564df323e6871e8d
rtjenkins <motphone16@gmail.com>
2017-02-11 10:07:14 -0800

    ver 4

--- HelloWorldV2.java
+++ HelloWorldV2.java
@@ -1,7 +1,7 @@
 public class HelloWorldV2 {
 	public static void main(String[] args) {
 	// Print a message to the terminal window
-	int ver = 3;
+	int ver = 4;
 	System.out.println("This is version " + ver + " of the project\n\n");
 
 	System.out.println("Hello World!!!\n\nThis is Armaan Tulsyani's hello to the entire Earth-1\n\nDo you know there is Earth-2?\n\n");
